c3730a30d88e02682911b04f00a991abdcee92f3
Tri Cao <tricao.dev@gmail.com>
2025-11-19 00:39:02 +0700

    update cmd

--- dataset.sh
+++ dataset.sh
@@ -1,8 +1,7 @@
 # Download dataset
-wget https://isic-archive.s3.amazonaws.com/challenges/2024/ISIC_2024_Training_Input.zip -O dataset.zip
+wget -P dataset/ https://isic-archive.s3.amazonaws.com/challenges/2024/ISIC_2024_Training_Input.zip
+wget -P dataset/ https://isic-archive.s3.amazonaws.com/challenges/2024/ISIC_2024_Training_GroundTruth.csv
 
 # Extract dataset
-## Linux/MacOS
-unzip -q dataset.zip -d dataset
-## Window
-Expand-Archive -Path "dataset.zip" -DestinationPath "dataset"
+unzip -q dataset/ISIC_2024_Training_Input.zip -d dataset/
+rm dataset/ISIC_2024_Training_Input.zip
