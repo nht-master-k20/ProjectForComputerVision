ff0708dd08d9c17c414d8e91c79fc47ba66013bc
ndh34 <nguyenduongghai07@gmail.com>
2025-11-21 02:11:20 +0700

    [haind] update

--- dataset.sh
+++
@@ -1,7 +0,0 @@
-# Download dataset
-wget -P dataset/ https://isic-archive.s3.amazonaws.com/challenges/2024/ISIC_2024_Training_Input.zip
-wget -P dataset/ https://isic-archive.s3.amazonaws.com/challenges/2024/ISIC_2024_Training_GroundTruth.csv
-
-# Extract dataset
-unzip -q dataset/ISIC_2024_Training_Input.zip -d dataset/
-rm dataset/ISIC_2024_Training_Input.zip
